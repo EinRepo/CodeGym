503664149b1eb85034d36f0f69908bc133c4d771
Ein <thiennguyenminh01@gmail.com>
2024-01-23 14:03:39 +0700

    DisplayGreeting

---
+++ GU101/GU1-W02/DisplayGreeting/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+Console.WriteLine("Enter your name: ");
+
+string yourName = Console.ReadLine() ?? "friend";
+//vscode warning yourName co the la null nen phai them ??
+
+Console.WriteLine("Hello: " + yourName);
