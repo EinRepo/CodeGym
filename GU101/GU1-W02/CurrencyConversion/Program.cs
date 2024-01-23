4b1facea704d7eef6bb33bcd7fc006005adc3918
Ein <thiennguyenminh01@gmail.com>
2024-01-23 14:45:25 +0700

    CurrencyConversion

---
+++ GU101/GU1-W02/CurrencyConversion/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+double rate = 23000;
+
+Console.Write("Enter the amount of USD: ");
+double usd = Convert.ToDouble(Console.ReadLine());
+double vnd = usd * rate;
+Console.WriteLine("Conversion rate is " + rate + " VND to 1 USD");
+Console.WriteLine("Equivalence in VND: " + vnd);
