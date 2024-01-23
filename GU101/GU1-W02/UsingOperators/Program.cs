02521df865897bd76a23d820236b084fb883afbe
Ein <thiennguyenminh01@gmail.com>
2024-01-23 13:39:56 +0700

    UsingOperators

---
+++ GU101/GU1-W02/UsingOperators/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+float width;
+float height;
+
+Console.Write("Enter width: ");
+width = float.Parse(Console.ReadLine() ?? "0.0");
+Console.Write("Enter height: ");
+height = float.Parse(Console.ReadLine() ?? "0.0");
+
+float area = width * height;
+Console.WriteLine("Area is: " + area);
