7cabc0e6d43c18b65bacd87fc3c9017a132a16ec
Ein <thiennguyenminh01@gmail.com>
2024-01-24 01:08:34 +0700

    Number2Word

---
+++ GU101/GU1-W02/Number2Word/Program.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Number2Word
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 0;
+            Console.WriteLine("Enter a positive integer, up to 3 digits:");
+            bool tryParseInt = int.TryParse(Console.ReadLine(), out number);
+            if (number < 0 || number > 999 || tryParseInt == false)
+                Console.WriteLine("Out of ability");
+            else
+            {
+                string result = ConvertToWords(number);
+                Console.WriteLine("Written Number: " + result);
+            }
+
+
+        }
+
+        static string ConvertToWords(int number)
+        {
+            if (number < 10)
+                return ConvertOnes(number);
+
+            if (number < 20)
+                return ConvertTeens(number);
+
+            if (number < 100)
+            {
+                int ones = number % 10;
+                int tens = number / 10;
+                return ConvertTens(tens) + " " + ConvertOnes(ones);
+            }
+
+            if (number < 1000)
+            {
+                int ones = number % 10;
+                int tens = (number / 10) % 10;
+                int hundreds = number / 100;
+                string result = ConvertOnes(hundreds) + " hundred";
+                if (number % 100 != 0)
+                    result += " and " + ConvertToWords(number % 100);
+                return result;
+            }
+            return "";
+        }
+        static string ConvertOnes(int number)
+        {
+            switch (number)
+            {
+                case 0:
+                    return "zero";
+                case 1:
+                    return "one";
+                case 2:
+                    return "two";
+                case 3:
+                    return "three";
+                case 4:
+                    return "four";
+                case 5:
+                    return "five";
+                case 6:
+                    return "six";
+                case 7:
+                    return "seven";
+                case 8:
+                    return "eight";
+                case 9:
+                    return "nine";
+                default:
+                    return "";
+            }
+        }
+
+        static string ConvertTeens(int number)
+        {
+            switch (number)
+            {
+                case 10:
+                    return "ten";
+                case 11:
+                    return "eleven";
+                case 12:
+                    return "twelve";
+                case 13:
+                    return "thirteen";
+                case 14:
+                    return "fourteen";
+                case 15:
+                    return "fifteen";
+                case 16:
+                    return "sixteen";
+                case 17:
+                    return "seventeen";
+                case 18:
+                    return "eighteen";
+                case 19:
+                    return "nineteen";
+                default:
+                    return "";
+            }
+        }
+
+        static string ConvertTens(int number)
+        {
+            switch (number)
+            {
+                case 2:
+                    return "twenty";
+                case 3:
+                    return "thirty";
+                case 4:
+                    return "forty";
+                case 5:
+                    return "fifty";
+                case 6:
+                    return "sixty";
+                case 7:
+                    return "seventy";
+                case 8:
+                    return "eighty";
+                case 9:
+                    return "ninety";
+                default:
+                    return "";
+            }
+        }
+    }
+}
+
---
+++ GU101/Projects/Buoi4_ThucHanhCauLenhDieuKien/Buoi4_ThucHanhCauLenhDieuKien/Program.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Buoi4_ThucHanhCauLenhDieuKien
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Bai_4
+            //Console.Write("Moi ban nhap tuoi: ");
+            //int tuoi = int.Parse(Console.ReadLine());
+
+            //if (tuoi > 0 && tuoi <= 2)
+            //{
+            //    Console.WriteLine("Tre so sinh");
+            //}
+            //else if (tuoi > 2 && tuoi <= 10)
+            //{
+            //    Console.WriteLine("Nhi dong");
+            //}
+            //else if (tuoi > 10 && tuoi <= 17)
+            //{
+            //    Console.WriteLine("Vi thanh nien");
+            //}
+            //else if (tuoi > 17 && tuoi <= 39)
+            //{
+            //    Console.WriteLine("Thanh nien");
+            //}
+            //else if (tuoi > 39 && tuoi <= 50)
+            //{
+            //    Console.WriteLine("Trung nien");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Cao nien");
+            //}
+
+            #endregion
+
+            #region Bai_3
+            //int currentYear = 2024;
+
+            //Console.Write("Nhập năm sản xuất của máy tính: ");
+            //int manufacturingYear = int.Parse(Console.ReadLine());
+
+            //int yearsDifference = currentYear - manufacturingYear;
+
+            //if (yearsDifference >= 15)
+            //{
+            //    Console.WriteLine("Đề xuất: Thay thế");
+            //}
+            //else if (yearsDifference >= 10 && yearsDifference < 15)
+            //{
+            //    Console.WriteLine("Đề xuất: Bảo trì");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Không có đề xuất");
+            //}
+
+            #endregion
+
+            #region Bai_1
+            //Console.Write("Enter the salary: ");
+            //double salary = double.Parse(Console.ReadLine());
+
+            //Console.Write("Enter the hours worked in a month: ");
+            //int hoursWorked = int.Parse(Console.ReadLine());
+
+            //double bonus = 0.0;
+
+            //if (hoursWorked >= 200)
+            //{
+            //    bonus = 0.2 * salary;
+            //}
+            //else if (hoursWorked >= 100 && hoursWorked < 200)
+            //{
+            //    bonus = 0.1 * salary;
+            //}
+
+            //double totalPayment = salary + bonus;
+
+            //Console.WriteLine("Bonus: " + bonus);
+            //Console.WriteLine("Total Payment: " + totalPayment);
+
+            #endregion
+
+            #region Bai_1_2
+
+            Console.Write("Enter integer N: ");
+            double n = double.Parse(Console.ReadLine());
+
+            // Kiểm tra N có phải số nguyên không
+            // Kiểm tra tính chẵn lẻ của N
+            // Kiểm tra tính chẵn dương của N
+            // Kiểm tra tính lẻ âm của N
+            // Kiểm tra N có phải số chính phương không
+            // Kiểm tra N có phải số đặc biệt không
+            bool isInteger = false;
+            bool isEven = false;
+            bool isOdd = false;
+            bool isPositiveEven = false;
+            bool isNegativeOdd = false;
+            bool isPerfectSquare = false;
+            bool isSpecial = false;
+            if (n == (int)n)
+            {
+                isInteger = true;
+                isEven = (n % 2 == 0);
+                isOdd = !isEven;
+                isPositiveEven = (n > 0 && n % 2 == 0);
+                isNegativeOdd = (n < 0 && n % 2 != 0);
+                isPerfectSquare = (Math.Sqrt(n) % 1 == 0);
+                if (n > 0 && n < 1000)
+                {
+                    int sumOfCubedDigits = 0;
+                    int temp = (int)n;
+                    while (temp != 0)
+                    {
+                        int digit = temp % 10;
+                        sumOfCubedDigits += (int)Math.Pow(digit, 3);
+                        temp /= 10;
+                    }
+                    isSpecial = (sumOfCubedDigits == n);
+                }
+            }
+
+            
+            
+            
+
+            Console.WriteLine("1. N is Integer: " + isInteger);
+            Console.WriteLine("2. N is even: " + isEven);
+            Console.WriteLine("   N is odd: " + isOdd);
+            Console.WriteLine("3. N is Positive Even: " + isPositiveEven);
+            Console.WriteLine("   N is Negative Odd: " + isNegativeOdd);
+            Console.WriteLine("4. N is Perfect Square: " + isPerfectSquare);
+            Console.WriteLine("5. N is Special (number is equal to the sums of its cubed digits): " + isSpecial);
+
+
+            #endregion
+            Console.ReadKey();
+        }
+    }
+}
