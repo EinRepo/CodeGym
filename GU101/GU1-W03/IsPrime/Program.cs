a35c09453137a9030cbb2f1241507b8bee910695
Ein <thiennguyenminh01@gmail.com>
2024-01-25 04:36:45 +0700

    IsPrime

---
+++ GU101/GU1-W03/IsPrime/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace IsPrime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            Console.WriteLine("Enter a number: ");
+            number = Convert.ToInt32(Console.ReadLine());
+
+            if (number < 2)
+                Console.WriteLine(number + " is not a prime");
+            else
+            {
+                int i = 2;
+                bool flag = true;
+                while (i <= Math.Sqrt(number))
+                {
+                    if (number % i == 0)
+                    {
+                        flag = false;
+                        break;
+                    }
+                    i++;
+                }
+                if (flag)
+                    Console.WriteLine(number + " is a prime");
+                else
+                    Console.WriteLine(number + " is not a prime");
+            }
+        }
+    }
+}
