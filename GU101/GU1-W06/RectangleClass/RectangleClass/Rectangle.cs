323266e0ae3441533c812bce473222f093de8104
EinRepo <thiennguyenminh01@gmail.com>
2024-04-04 14:01:43 +0700

    RectangleClass

---
+++ GU101/GU1-W06/RectangleClass/RectangleClass/Rectangle.cs
@@ -0,0 +1,31 @@
+﻿namespace RectangleClass
+{
+    public class Rectangle
+    {
+        double width, height;
+
+        public Rectangle()
+        {
+        }
+
+        public Rectangle(double width, double height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+        public double GetArea()
+        {
+            return this.width * this.height;
+        }
+
+        public double GetPerimeter()
+        {
+            return (this.width + this.height) * 2;
+        }
+
+        public string Display()
+        {
+            return "Rectangle{" + "width=" + width + ", height=" + height + "}";
+        }
+    }
+}
