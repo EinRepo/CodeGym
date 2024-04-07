b850db6f479db6f637de7a7e2b2f85dd5c5bfdf7
EinRepo <thiennguyenminh01@gmail.com>
2024-04-07 17:55:58 +0700

    StaticMethod

---
+++ GU101/GU1-W06/StaticMethod/StaticMethod/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticMethod
+{
+    internal partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Student.Change();//calling change method
+                             //creating objects
+            Student s1 = new Student(111, "Hoang");
+            Student s2 = new Student(222, "Khanh");
+            Student s3 = new Student(333, "Nam");
+            //calling display method
+            s1.Display();
+            s2.Display();
+            s3.Display();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ GU101/GU1-W06/StaticMethod/StaticMethod/Student.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace StaticMethod
+{
+    public class Student
+    {
+        private int rollno;
+        private string name;
+        private static string college = "BBDIT";
+
+        //constructor to initialize the variable
+        public Student(int r, string n)
+        {
+            rollno = r;
+            name = n;
+        }
+
+        //static method to change the value of static variable
+
+        public static void Change()
+        {
+            college = "CODEGYM";
+        }
+
+        //method to display values
+        public void Display()
+        {
+            Console.WriteLine(rollno + " " + name + " " + college);
+        }
+    }
+}
