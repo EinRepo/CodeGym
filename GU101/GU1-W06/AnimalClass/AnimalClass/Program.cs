e6991fcc3d81b640b644fb37d042f27eabb0ad93
EinRepo <thiennguyenminh01@gmail.com>
2024-04-07 17:38:30 +0700

    AnimalClass

---
+++ GU101/GU1-W06/AnimalClass/AnimalClass/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat cat = new Cat("20kg", "1.5", "kitty");
+            cat.PrintInfo();
+            Console.ReadKey();
+        }
+    }
+}
