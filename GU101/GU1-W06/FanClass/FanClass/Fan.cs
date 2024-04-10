c69a5babca14a862d540fd9598b1eff41de8aaae
EinRepo <thiennguyenminh01@gmail.com>
2024-04-10 15:20:24 +0700

    FanClass

---
+++ GU101/GU1-W06/FanClass/FanClass/Fan.cs
@@ -0,0 +1,66 @@
+﻿namespace FanClass
+{
+    public class Fan
+    {
+        private const int SLOW = 1;
+        private const int MEDIUM = 2;
+        private const int FAST = 3;
+
+        private int speed;
+        private bool on;
+        private double radius;
+        private string color;
+
+        public Fan()
+        {
+            speed = SLOW;
+            on = false;
+            radius = 5;
+            color = "blue";
+        }
+
+        public Fan(int speed, bool on, double radius, string color)
+        {
+            this.speed = speed;
+            this.on = on;
+            this.radius = radius;
+            this.color = color;
+        }
+
+        public int Speed
+        {
+            get { return speed; }
+            set { speed = value; }
+        }
+
+        public bool On
+        {
+            get { return on; }
+            set { on = value; }
+        }
+
+        public double Radius
+        {
+            get { return radius; }
+            set { radius = value; }
+        }
+
+        public string Color
+        {
+            get { return color; }
+            set { color = value; }
+        }
+
+        public override string ToString()
+        {
+            if (on)
+            {
+                return $"Speed: {speed}, Color: {color}, Radius: {radius}, Fan is on";
+            }
+            else
+            {
+                return $"Color: {color}, Radius: {radius}, Fan is off";
+            }
+        }
+    }
+}
---
+++ GU101/GU1-W06/FanClass/FanClass/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FanClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Fan fan1 = new Fan(3, true, 10, "yellow");
+            Fan fan2 = new Fan(2, false, 5, "blue");
+
+            Console.WriteLine("Fan 1 - " + fan1.ToString());
+            Console.WriteLine("Fan 2 - " + fan2.ToString());
+
+            Console.ReadKey();  
+        }
+    }
+}
