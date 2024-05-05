fc6b98209b406e7f333c27e0e5a1dba148b4040b
EinRepo <thiennguyenminh01@gmail.com>
2024-05-06 02:03:44 +0700

    IComparable

---
+++ GU101/GU1-W07/IComparable/IComparable/ComperableCircle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace IComparable
+{
+    public class ComparableCircle : Circle, IComparable<ComparableCircle>
+    {
+
+        public ComparableCircle() { }
+        public ComparableCircle(double radius) : base(radius) { }
+        public ComparableCircle(double radius, string color, bool filled) : base(radius, color, filled) { }
+
+        public int CompareTo(ComparableCircle o)
+        {
+            if (getRadius() > o.getRadius()) return 1;
+            else if (getRadius() < o.getRadius()) return -1;
+            else return 0; 
+        }
+    }
+}
---
+++ GU101/GU1-W07/IComparable/IComparable/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IComparable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ComparableCircle[] circles = new ComparableCircle[3];
+            circles[0] = new ComparableCircle(3.6);
+            circles[1] = new ComparableCircle();
+            circles[2] = new ComparableCircle(3.5, "indigo", false);
+
+            Console.WriteLine("Pre-sorted:");
+            foreach (ComparableCircle circle in circles)
+            {
+                Console.WriteLine(circle);
+            }
+
+            Array.Sort(circles);
+
+            Console.WriteLine("After-sorted:");
+            foreach (ComparableCircle circle in circles)
+            {
+                Console.WriteLine(circle);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
