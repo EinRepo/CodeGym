34ccb512453c89e536a4d58d805620114fe876aa
EinRepo <thiennguyenminh01@gmail.com>
2024-05-19 02:09:26 +0700

    Point2DAndPoint3D

---
+++ GU101/GU1-W06/Point2DAndPoint3D/Point2DAndPoint3D/Point2D.cs
@@ -0,0 +1,44 @@
+﻿namespace Point2DAndPoint3D
+{
+    public class Point2D
+    {
+        protected float x = 0.0f;
+        protected float y = 0.0f;
+
+        // Constructors
+        public Point2D()
+        {
+        }
+
+        public Point2D(float x, float y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        // Getters
+        public float getX()
+        {
+            return x;
+        }
+
+        public float getY()
+        {
+            return y;
+        }
+
+        // Setters
+        public void setX(float x)
+        {
+            this.x = x;
+        }
+
+        public void setY(float y)
+        {
+            this.y = y;
+        }
+
+        
+
+    }
+}
---
+++ GU101/GU1-W06/Point2DAndPoint3D/Point2DAndPoint3D/Point3D.cs
@@ -0,0 +1,49 @@
+﻿namespace Point2DAndPoint3D
+{
+    public class Point3D : Point2D
+    {
+        private float z = 0.0f;
+
+        // Constructors
+        public Point3D() : base() { }
+
+        public Point3D(float x, float y, float z) : base(x, y)
+        {
+            this.z = z;
+        }
+
+        // Getter
+        public float GetZ()
+        {
+            return z;
+        }
+
+        // Setter
+        public void SetZ(float z)
+        {
+            this.z = z;
+        }
+
+        // Set x, y, and z
+        public void SetXYZ(float x, float y, float z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        // Get x, y, and z as an array
+        public float[] GetXYZ()
+        {
+            return new float[] { x, y, z };
+        }
+
+        // ToString method
+        public override string ToString()
+        {
+            return $"({x},{y},{z})";
+        }
+    }
+
+
+}
