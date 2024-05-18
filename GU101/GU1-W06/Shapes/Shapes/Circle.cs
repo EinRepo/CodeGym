32907f9da087693958abe404ce7ec3da26bc4156
EinRepo <thiennguyenminh01@gmail.com>
2024-05-18 13:48:01 +0700

    Shapes

---
+++ GU101/GU1-W06/Shapes/Shapes/Circle.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace Shapes
+{
+    public class Circle : Shape
+
+    {
+
+        private double radius = 1.0;
+
+        public Circle()
+
+        {
+
+        }
+
+        public Circle(double radius)
+
+        {
+
+            this.radius = radius;
+
+        }
+
+        public Circle(double radius, String color, bool filled) : base(color, filled)
+
+        {
+
+            this.radius = radius;
+
+        }
+
+        public double GetRadius()
+
+        {
+
+            return radius;
+
+        }
+
+        public void SetRadius(double radius)
+
+        {
+
+            this.radius = radius;
+
+        }
+
+        public double GetArea()
+
+        {
+
+            return radius * radius * Math.PI;
+
+        }
+
+        public double GetPerimeter()
+
+        {
+
+            return 2 * radius * Math.PI;
+
+        }
+
+        public override String ToString()
+
+        {
+
+            return "A Circle with radius ="
+
+                    + GetRadius()
+
+                    + ", which is a subclass of "
+
+                    +base.ToString();
+
+        }
+
+    }
+}
---
+++ GU101/GU1-W06/Shapes/Shapes/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shapes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Square square = new Square();
+
+            Console.WriteLine(square);
+
+            square = new Square(2.3);
+
+            Console.WriteLine(square);
+
+            square = new Square(5.8, "yellow", true);
+
+            Console.WriteLine(square);
+            Console.ReadKey();
+        }
+    }
+}
