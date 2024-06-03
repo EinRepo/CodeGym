ea2d66d23ce839620a32e5d1c5dce9b33daa2a0f
EinRepo <thiennguyenminh01@gmail.com>
2024-06-04 00:14:04 +0700

    IndexOutOfRangeExceptionClass

---
+++ GU101/GU1-W08/IndexOutOfRangeException/IndexOutOfRangeException/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IndexOutOfRangeExceptionClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                List<char> characters = new List<char>();
+                characters.InsertRange(0, new char[] { 'a', 'b', 'c', 'd', 'e', 'f' });
+                int value = characters[6];
+
+
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                System.Console.WriteLine(ex);
+
+            }
+        }
+    }
+}
